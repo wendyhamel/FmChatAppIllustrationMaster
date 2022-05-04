3bed7efcd8a5b723c72efb17caf37a7984d127ee
wendyhamel <whamel@it4web.nl>
2022-05-04 12:10:38 +0200

    Working on styling

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -5,21 +5,19 @@ tailwind.config = {
         },
         extend: {
             colors: {
-                // Primary
-                // text
-                'violet-pale': '276, 100%, 81%', // sub-heading at the top of the app UI
-                'violet-mod': '276, 55%, 52%', // chat on the left
-                'violet-dark' : '271, 15%, 43%', // chat on the right
-                'violet-dark-des': '271, 36%, 24%', // main heading and submit button background
-                'violet-dark-gray': '270, 7%, 64%', // paragraph
-                'grayish-blue': '206, 6%, 79%', // placeholder text
+                'violet-pale': 'hsl(276, 100%, 81%)', // sub-heading at the top of the app UI
+                'violet-mod': 'hsl(276, 55%, 52%)', // chat on the left
+                'violet-dark' : 'hsl(271, 15%, 43%)', // chat on the right
+                'violet-dark-des': 'hsl(271, 36%, 24%)', // main heading and submit button background
+                'violet-dark-gray': 'hsl(270, 7%, 64%)', // paragraph
+                'grayish-blue': 'hsl(206, 6%, 79%)', // placeholder text
 
-                'magenta-light': '293, 100%, 63%', // background colors and gradients
-                'violet-light': '264, 100%, 61%', // background colors and gradients
+                'magenta-light': 'hsl(293, 100%, 63%)', // background colors and gradients
+                'violet-light': 'hsl(264, 100%, 61%)', // background colors and gradients
 
-                'White': '(0, 0%, 100%',
-                'violet-light-gray': '(270, 20%, 96%', // app background
-                'magenta-very-light': '(289, 100%, 72%', // radio button outline
+                'White': 'hsl(0, 0%, 100%)',
+                'violet-light-gray': 'hsl(270, 20%, 96%)', // app background
+                'magenta-very-light': 'hsl(289, 100%, 72%)', // radio button outline
             }
         }
     }
